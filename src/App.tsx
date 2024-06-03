751a04d9cb2caf0c8663bacb9b66089b9e9a46fc
Isaac Torres <itorres@front2go.com>
2024-06-03 12:29:46 -0500

    Pasando un listado de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import Titulo from './components/Titulo'
 // import Card from "./components/Card";
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el parrafo de texto" />
+      <List />
     </Card>
   );
 }
