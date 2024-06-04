a9cb9db86359ef5f8f64ea34682ffda116f7ab55
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-06-04 03:01:00 -0500

    usar onclick con una función externa pasando un evento

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,17 @@
+import { MouseEvent } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
+  const handleClick = (e: MouseEvent) => {
+    console.log(e);
+  };
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">
+      {data.map((elemento, indice) => (
+        <li onClick={handleClick} key={elemento} className="list-group-item">
           {elemento}
         </li>
       ))}
