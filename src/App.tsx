7229fe26dd0d618c3432eb1ae4fd9f36a3bc61ea
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-06-04 04:51:42 -0500

    pasando funciones como props

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = ['Goku','Tanjiro','Eren']
+  const list = ["Goku", "Tanjiro", "Eren"];
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log("mostrando", elemento);
+  };
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el parrafo de texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
+function List({ data, onSelect }: Props) {
   const [index, setIndex] = useState(1);
   const handleClick = (e: string, indice: number) => {
     setIndex(indice);
+    onSelect?.(e); //Funcionalidad de TS donde si es que esta funcion se encuentra definida se ejecuta.
     console.log(`Elemento ${e} Indice ${indice}`);
   };
   return (
