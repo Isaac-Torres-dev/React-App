f7aae6a8dc10f799b91f27896635f5735d9437ac
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-06-04 05:12:52 -0500

    Truthy y falsy en React
    se ve el short circuit operator

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import List from "./components/List";
 
 function App() {
   const list = ["Goku", "Tanjiro", "Eren"];
+  const list2: string[] = [];
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
@@ -14,6 +15,16 @@ function App() {
 
   return (
     <Card>
+      {/* {"" && "string vacio"} //short circuit operator
+      {undefined && "indefinido"}
+      {null && "nulo"}
+      {false && "falso"}
+      {0 && "Cero"} debemos evitar evaluar con cero*/}
+      {list.length && "mi lista "}
+      <br />
+      {list2.length !== 0 && "mi lista 2"} {/* evitar hacer esto */}
+      <br />
+      {"Hola mundo" && "string Hola mundo"}
       <CardBody title="Hola Mundo" text="Este es el parrafo de texto" />
       <List data={list} onSelect={handleSelect} />
       <List data={list} onSelect={handleSelect2} />
