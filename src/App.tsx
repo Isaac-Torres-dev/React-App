14f11ecd3e3d69cdea6d71aed688016af7febda0
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-06-04 02:42:10 -0500

    pasar datos desde un componente padre

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
+  const list = ['Goku','Tanjiro','Eren']
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el parrafo de texto" />
-      <List />
+      <List data={list} />
     </Card>
   );
 }
