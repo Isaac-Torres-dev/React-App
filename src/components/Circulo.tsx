2c29b287c13c6dd01d2d320ff785f5bd1b4f9776
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-06-12 06:09:14 -0500

    Comentarios utiles del curso

---
+++ src/components/Circulo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// Ctrl + Shift + R nos abre los snnipets de ract
+type Props = {}
+
+function Circulo({}: Props) {
+  return (
+    <div>Circulo</div>
+  )
+}
+
+export default Circulo
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,17 +8,17 @@ type Props = {
 };
 
 function List({ data, onSelect }: Props) {
-  const [index, setIndex] = useState(1);
-  const handleClick = (e: string, indice: number) => {
+  const [index, setIndex] = useState(1); // Solo cuando usemos el setIndex es cuando la funcion de List completa se ejecuta y react renderiza de nuevo
+  const handleClick = (event: string, indice: number) => { // 
     setIndex(indice);
-    onSelect?.(e); //Funcionalidad de TS donde si es que esta funcion se encuentra definida se ejecuta.
-    console.log(`Elemento ${e} Indice ${indice}`);
+    onSelect?.(event); //Funcionalidad de TS donde si es que esta funcion se encuentra definida se ejecuta.
+    console.log(`Elemento ${event} Indice ${indice}`);
   };
   return (
     <ul className="list-group">
       {data.map((elemento, indice) => (
         <li
-          onClick={() => handleClick(elemento, indice)}
+          onClick={() => handleClick(elemento, indice)} // onClick es una propiedad de html, las props como esta siempre reciben un evento que a su vez en un objeto
           key={elemento}
           className={`list-group-item ${index == indice ? "active" : ""}`}
         >
