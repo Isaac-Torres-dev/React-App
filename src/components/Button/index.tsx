c873b898681e197d6e16e5b6c55e6754f44b4dd8
Isaac-Torres-dev <isaac.torres.dev@gmail.com>
2024-07-08 10:56:19 -0500

    05 - Módulos de CSS

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
-import './Button.css'
+import styles from './Button.module.css'
 
 type Props = {
   children: ReactNode
@@ -7,13 +7,21 @@ type Props = {
   onClick: () => void
 }
 
+console.log(styles)
+
 function Buttom({ children, isLoading, onClick }: Props) {
+  const className = [
+    `btn btn-${isLoading ? 'secondary' : 'primary'}`,
+    styles.button,
+  ].join(' ')
+
   return (
     <button
       onClick={onClick}
       disabled={isLoading}
       type='button'
-      className={`btn btn-${isLoading ? 'secondary' : 'primary'}`}
+      className={className}
+      // className={[styles.button, styles.padded].join(' ')}
     >
       {isLoading ? 'Cargando...' : children}
     </button>
