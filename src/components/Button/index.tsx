9efffb7ec7dbbde2651e5627c8ff398cc4d333a5
Isaac-Torres <itorres@front2go.com>
2024-10-07 13:20:40 -0500

    No recuerdo

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react'
 import styles from './Button.module.css'
+import styled from 'styled-components'
+
+const Btn = styled.button`
+  background-color: red;
+  padding: 15px 20px;
+`
 
 type Props = {
   children: ReactNode
@@ -10,21 +16,21 @@ type Props = {
 console.log(styles)
 
 function Buttom({ children, isLoading, onClick }: Props) {
-  const className = [
-    `btn btn-${isLoading ? 'secondary' : 'primary'}`,
-    styles.button,
-  ].join(' ')
+  // const className = [
+  //   `btn btn-${isLoading ? 'secondary' : 'primary'}`,
+  //   styles.button,
+  // ].join(' ')
 
   return (
-    <button
+    <Btn
       onClick={onClick}
       disabled={isLoading}
       type='button'
-      className={className}
+      // className={className}
       // className={[styles.button, styles.padded].join(' ')}
     >
       {isLoading ? 'Cargando...' : children}
-    </button>
+    </Btn>
   )
 }
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,33 +1,33 @@
 // import { MouseEvent } from "react";
 
-import { useState } from "react";
+import { useState } from 'react'
 
 type Props = {
-  data: string[];
-  onSelect?: (elemento: string) => void;
-};
+  data: string[]
+  onSelect?: (elemento: string) => void
+}
 
 function List({ data, onSelect }: Props) {
-  const [index, setIndex] = useState(1); // Solo cuando usemos el setIndex es cuando la funcion de List completa se ejecuta y react renderiza de nuevo
+  const [index, setIndex] = useState(1) // Solo cuando usemos el setIndex es cuando la funcion de List completa se ejecuta y react renderiza de nuevo
   const handleClick = (event: string, indice: number) => {
     //
-    setIndex(indice);
-    onSelect?.(event); //Funcionalidad de TS donde si es que esta funcion se encuentra definida se ejecuta.
-    console.log(`Elemento ${event} Indice ${indice}`);
-  };
+    setIndex(indice)
+    onSelect?.(event) //Funcionalidad de TS donde si es que esta funcion se encuentra definida se ejecuta.
+    console.log(`Elemento ${event} Indice ${indice}`)
+  }
   return (
-    <ul className="list-group">
+    <ul className='list-group'>
       {data.map((elemento, indice) => (
         <li
           onClick={() => handleClick(elemento, indice)} // onClick es una propiedad de html, las props como esta siempre reciben un evento que a su vez en un objeto
-          key={elemento}
-          className={`list-group-item ${index == indice ? "active" : ""}`}
+          key={`${elemento}-${indice}`}
+          className={`list-group-item ${index == indice ? 'active' : ''}`}
         >
           {elemento}
         </li>
       ))}
     </ul>
-  );
+  )
 }
 
-export default List;
+export default List
